3cb42ee1da4297d461d9176739fb8b397aa7616b
sampath <90138346+sn-pegasus@users.noreply.github.com>
2023-02-23 10:06:48 +0530

    game
     In case of any error change the class name of q1 to tic tac toe

---
+++ TIC TAC TOE.java
@@ -0,0 +1,13 @@
+package AI;
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+    }
+}
